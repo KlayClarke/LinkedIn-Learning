4fe358b2e8c1554e59a14531cfe3b9976226201e
Klay Clarke <clarkeklay@gmail.com>
2022-10-29 09:43:14 -0400

    complete casting interfaces

---
+++ C# Interfaces and Generics/CastingInterfaces/Classes/Document.cs
@@ -0,0 +1,33 @@
+namespace CastingInterfaces
+{
+    // TODO: Define an IStorable interface that provides the ability to load and save info for an object
+    
+    // TODO: Implement IStorable on the Document class
+    class Document : IStorable
+    {
+        private string name;
+        
+        public Document(string s)
+        {
+            name = s;
+            Console.WriteLine($"Created a document with name {name}");
+            
+        }
+        
+        // TODO: Implement the IStorable interface methods and properties
+        public void Save()
+        {
+            Console.WriteLine("Saving the document!");
+        }
+        
+        public void Load()
+        {
+            Console.WriteLine("Loading the document!");
+        }
+        
+        public bool NeedsSave 
+        {
+            get; set;
+        }
+    }
+}
---
+++ C# Interfaces and Generics/CastingInterfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CastingInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document d = new Document("Test Document");
+            
+            // TODO: Use the 'is' operator
+            if (d is IStorable)
+            {
+                d.Save();
+            }
+            
+            // TODO: Use the 'as' operator
+            IStorable istor = d as IStorable;
+            if (istor != null)
+            {
+                istor.Load();
+            }
+            
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
