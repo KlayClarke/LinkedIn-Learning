2c554cff229d15fb02b427c00431afd82f5f2239
Klay Clarke <clarkeklay@gmail.com>
2022-10-29 09:37:38 -0400

    complete basic interfaces

---
+++ C# Interfaces and Generics/BasicInterfaces/Interfaces/IStorable.cs
@@ -0,0 +1,14 @@
+namespace BasicInterfaces
+{
+    interface IStorable
+    // doesn't contain logic, but defines the properties
+    // cant be instantiated by self
+    {
+        void Save();
+        
+        void Load();
+        
+        bool NeedsSave { get; set; }
+        
+    }
+}
---
+++ C# Interfaces and Generics/BasicInterfaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BasicInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document d = new Document("Test Document");
+            
+            // TODO: Exercise the IStorable interface
+            d.Load();
+            d.Save();
+            
+            Console.WriteLine("\nPress Enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
